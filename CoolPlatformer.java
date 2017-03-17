c15edcc7719063dbe7254fa1cb1b179cc1528d36
unyieldingphoenix <patrick.au@student.tdsb.on.ca>
2017-03-17 19:51:42 -0400

    Add GameScreen menu
    JF.setLocationRelativeTo() comes after JF.setSize();

--- CoolPlatformer.java
+++ CoolPlatformer.java
@@ -6,25 +6,28 @@
 *******/
 import java.awt.*;
 import java.awt.event.*;
+
 import java.io.*;
 import java.util.*;
+
 import javax.swing.*;
 
 public class CoolPlatformer {
 	public static JFrame JF;
 	public static final Dimension dim = new Dimension(800, 600);
 
-	public static JPanel[] screen;
+	public static JPanel[] menu;
 
 	public CoolPlatformer() {
 		JF = new JFrame("Cool Platformer");
 		JF.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		JF.setLayout(new BorderLayout());
-		JF.setLocationRelativeTo(null); // Puts JF at Centre of Screen
 		JF.setResizable(false);
 		JF.setSize(dim);
+		JF.setLocationRelativeTo(null); // Puts JF at Centre of Screen
 
-		screen = new JPanel[1]; // Number of Menu Screens
+		menu = new JPanel[1]; // Number of Menu Screens
+		menu[0] = new GameScreen();
 
 		JF.setVisible(true);
 	}	// end constructor
