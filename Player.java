82472651eeee5c21192fbec405e223d8c6ada38d
unyieldingphoenix <patrick.au@student.tdsb.on.ca>
2017-03-18 03:04:51 -0400

    Add accelerate and move methods

--- Player.java
+++ Player.java
@@ -17,14 +17,19 @@ class Player extends Character {
 	public void advance() {
 		this.vel.add(this.acc);
 
-		if(vel.getY() > 0.0) { // Only Checks "Below" the Player
+		if(vel.getY() > 0.0) { // Only Checks "Below" the Player for Solid Earth
 			posInt[0] = (int)Math.round(pos.getX() + Block.getSize() / 2) / Block.getSize();
 			posInt[1] = (int)Math.round(pos.getY() + Block.getSize() / 2) / Block.getSize() + 1;
 			if(GameScreen.getBlocks(posInt[1], posInt[0]).getBlock() == 1)
-				this.vel.setY(0.0); // Block below is Solid Earth
+				// Reduce Velocity to Size of Gap between Player and Block to avoid going through the ground
+				this.vel.setY(Math.min(posInt[1] * Block.getSize() - pos.getY() - Block.getSize(), vel.getY()));
 		}	// end if
 		this.pos.add(this.vel);
 	}	// end method advance
+
+	public void accl(final Vector velo) {
+		this.vel.add(velo);
+	}	// end method accl
 
 	@Override // Superclass: Character
 	public void draw(Graphics g) {
@@ -32,4 +37,12 @@ class Player extends Character {
 		g2D = (Graphics2D)g;
 		g2D.drawImage(Images.demo[2], (int)Math.round(pos.getX()), (int)Math.round(pos.getY()), Block.getSize(), Block.getSize(), null);
 	}	// end method draw
+
+	public final Vector getVel() {
+		return this.vel;
+	}	// end method getVel
+
+	public void move(final Vector disp) {
+		this.pos.add(disp);
+	}	// end method move
 }	// end class
