108904b9ef3a4c5aaa9b5e660431dc5fc4ffa8ff
unyieldingphoenix <patrick.au@student.tdsb.on.ca>
2017-03-17 22:14:46 -0400

    Load images and more JFrame config

--- CoolPlatformer.java
+++ CoolPlatformer.java
@@ -5,32 +5,43 @@
 * note: Cool Platformer
 *******/
 import java.awt.*;
-import java.awt.event.*;
-
-import java.io.*;
-import java.util.*;
 
 import javax.swing.*;
 
 public class CoolPlatformer {
-	public static JFrame JF;
-	public static final Dimension dim = new Dimension(800, 600);
+	// Offset so JF Dimensions = JP
+	private static final int offX = 6;
+	private static final int offY = 29;
 
+	public static final Dimension size = new Dimension(800, 600);
+	public static final Dimension sizeJF = new Dimension(size.width+offX, size.height+offY);
+	public static JFrame JF;
 	public static JPanel[] menu;
 
 	public CoolPlatformer() {
 		JF = new JFrame("Cool Platformer");
 		JF.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		JF.setLayout(new BorderLayout());
+		JF.setPreferredSize(sizeJF);
 		JF.setResizable(false);
-		JF.setSize(dim);
-		JF.setLocationRelativeTo(null); // Puts JF at Centre of Screen
+
+		try {
+			Images.loadAll();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}	// end catch
 
 		menu = new JPanel[1]; // Number of Menu Screens
-		menu[0] = new GameScreen();
+		menu[0] = new GameScreen(size);
+		JF.add(menu[0], BorderLayout.CENTER);
 
+		JF.pack();
+		JF.setLocationRelativeTo(null); // Puts JF at Centre of Screen
 		JF.setVisible(true);
-	}	// end constructor
+
+		((GameScreen)menu[0]).init();
+	}	// end constructor()
 
 	public static void main(String[] args) {
 		new CoolPlatformer();
