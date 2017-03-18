6d1cbb216a35b00fa5a5e70c5ca9d60917aa0207
unyieldingphoenix <patrick.au@student.tdsb.on.ca>
2017-03-18 01:20:11 -0400

    Stop Player from falling through blocks

--- Player.java
+++ Player.java
@@ -16,6 +16,13 @@ class Player extends Character {
 	@Override // Superclass: Character
 	public void advance() {
 		this.vel.add(this.acc);
+
+		if(vel.getY() > 0.0) { // Only Checks "Below" the Player
+			posInt[0] = (int)Math.round(pos.getX() + Block.getSize() / 2) / Block.getSize();
+			posInt[1] = (int)Math.round(pos.getY() + Block.getSize() / 2) / Block.getSize() + 1;
+			if(GameScreen.getBlocks(posInt[1], posInt[0]).getBlock() == 1)
+				this.vel.zeroY(); // Block below is Solid Earth
+		}	// end if
 		this.pos.add(this.vel);
 	}	// end method advance
 
@@ -23,6 +30,6 @@ class Player extends Character {
 	public void draw(Graphics g) {
 		// Hardcode image for Demo
 		g2D = (Graphics2D)g;
-		g2D.drawImage(Images.demo[2], this.pos.getIntX(), this.pos.getIntY(), Block.getSize(), Block.getSize(), null);
+		g2D.drawImage(Images.demo[2], (int)Math.round(pos.getX()), (int)Math.round(pos.getY()), Block.getSize(), Block.getSize(), null);
 	}	// end method draw
 }	// end class
