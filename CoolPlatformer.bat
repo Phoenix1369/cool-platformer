a06553c00ef5073eb1ae596205cf39df1e8662b2
unyieldingphoenix <patrick.au@student.tdsb.on.ca>
2017-03-27 23:31:53 -0400

    Suppress REM comments

--- CoolPlatformer.bat
+++ CoolPlatformer.bat
@@ -1,9 +1,9 @@
 @echo off
 cd %~dp0
 del *.class
-@echo on
 REM "cd"s into current Disk and Path
 REM Deletes Existing '.class' files
+@echo on
 javac CoolPlatformer.java
 pause
 java CoolPlatformer
