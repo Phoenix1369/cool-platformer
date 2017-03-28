e64c8bb26c60d77804c53c10075c0353ab860481
unyieldingphoenix <patrick.au@student.tdsb.on.ca>
2017-03-28 00:44:04 -0400

    Add tinted field background images

--- Images.java
+++ Images.java
@@ -14,7 +14,9 @@ import javax.imageio.*;
 
 class Images 
 {
+	// First Set of Images
 	public static BufferedImage[] demo;
+	public static BufferedImage[] tint; // Field Tint
 	// Temporary Variables
 	private static BufferedImage temImg, temImg2;
 	private static Graphics2D g2D;
@@ -34,5 +36,18 @@ class Images
 			g2D.drawImage(temImg2, 0, 0, null);
 			g2D.dispose();
 		}
+		// Tints an Image
+		tint = new BufferedImage[2];
+		for(int i = 0; i < tint.length; ++i)
+		{
+			tint[i] = new BufferedImage(demo[i].getWidth(), demo[i].getHeight(), BufferedImage.TYPE_INT_ARGB);
+			g2D = tint[i].createGraphics();
+			g2D.drawImage(demo[i], 0, 0, null);
+			g2D.setComposite(AlphaComposite.SrcAtop); // Composites (src in des) onto destination
+			g2D.setColor(new Color(255, 0, 0, 100)); // Opacity: 100 / 255 Percent
+			g2D.fillRect(0, 0, tint[i].getWidth(), tint[i].getHeight());
+			g2D.dispose();
+		}
+		
 	}	// end method loadAll
 }	// end class Images
