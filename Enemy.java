6203444011f827a58f644d4d714817c38f950d4e
unyieldingphoenix <patrick.au@student.tdsb.on.ca>
2017-06-01 13:43:04 -0400

    Create Enemy.java

---
+++ Enemy.java
@@ -0,0 +1,24 @@
+/******
+* name: Patrick Au, James Long
+* date: March 2017
+* code: ICS4U1
+* note: Enemy entity character
+*******/
+import java.awt.*;
+
+class Enemy extends Entity
+{
+	Enemy()
+	{
+		super();
+	}	// end constructor()
+
+	public void advance()
+	{
+	}	// end method advance
+
+	public void draw(Graphics g)
+	{
+	}	// end method draw
+
+}	// end class Enemy
