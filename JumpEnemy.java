84f5f0e86b1a2ba2066a440a4a2c3eac11a27e01
unyieldingphoenix <patrick.au@student.tdsb.on.ca>
2017-06-01 13:45:47 -0400

    Create JumpEnemy.java

---
+++ JumpEnemy.java
@@ -0,0 +1,16 @@
+/******
+* name: Patrick Au, James Long
+* date: March 2017
+* code: ICS4U1
+* note: JumpEnemy character - jumps follow main character's jumps
+*******/
+import java.awt.*;
+
+class JumpEnemy extends Entity
+{
+	JumpEnemy(double x, double y)
+	{
+		super();
+		this.pos(x, y);
+	}	// end constructor(double,double)
+}	// end class Entity
