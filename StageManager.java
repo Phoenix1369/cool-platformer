428b0e614b88190ab85d9e1d3182c6ed9bd72578
James L <jameslong0410@gmail.com>
2017-06-03 23:18:33 -0400

    Create class for managing maps

---
+++ StageManager.java
@@ -0,0 +1,16 @@
+/******
+* name: Patrick Au, James Long
+* date: March 2017
+* code: ICS4U1
+* note: Stage Loader Class
+*******/
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.util.StringTokenizer;
+import javax.swing.*;
+
+class StageManager 
+{	
+	
+}	// end class StageManager
