c61d526039a81b3ce64434b824ce50c0aecd7587
unyieldingphoenix <patrick.au@student.tdsb.on.ca>
2017-06-09 12:57:23 -0400

    Read images in a specified format

--- Images.java
+++ Images.java
@@ -13,45 +13,54 @@ import java.io.File;
 import javax.imageio.*;
 
 class Images 
-{
-	// First Set of Images
-	public static BufferedImage[] demo;
-	public static BufferedImage[][] tint; // Field Tint
+{	// Loads Image Sets
+	public static BufferedImage[] sprites;
+	public static BufferedImage[][] tiles;
+	public static BufferedImage[][][] tint; // tint[i][j][k]: tiles[i][j] tintBG[k]
 	public static final Color[] tintBG = {
-		new Color(255, 0, 0, 100), new Color(255, 255, 0, 100),
-		new Color(0, 0, 0, 0), new Color(0, 255, 0, 100)
+		new Color(255,   0,   0, 100), new Color(255, 255, 0, 100),
+		new Color(255, 255, 255,   0), new Color(  0, 255, 0, 100)
 	};
+	public static final String imgDir = new File("include", "demo").getPath();
 	// Temporary Variables
 	private static BufferedImage temImg, temImg2;
 	private static Graphics2D g2D;
 
 	public static void loadAll() throws IOException 
-	{
-		// Loads Demo Images
-		demo = new BufferedImage[3];
-		temImg = ImageIO.read(new File("include/demo/demotiles.png"));
-		for(int j = 0; j < demo.length; ++j)
+	{	// Loads Sprites
+		sprites = new BufferedImage[2];
+		temImg = ImageIO.read(new File(imgDir, "sprites.png"));
+		for(int i = 0; i < sprites.length; ++i)
+			loadSubimage(sprites, i, 0, i * Block.getLen(), Block.getLen(), Block.getLen());
+		// Loads Tiles
+		tiles = new BufferedImage[2][2];
+		temImg = ImageIO.read(new File(imgDir, "tiles.png"));
+		for(int i = 0; i < tiles.length; ++i)
+			for(int j = 0; j < tiles[i].length; ++j)
+				loadSubimage(tiles[i], j, j * Block.getLen(), i * Block.getLen(), Block.getLen(), Block.getLen());
+		// Tints the Tiles
+		tint = new BufferedImage[2][2][4];
+		for(int i = 0; i < tint.length; ++i)
+		for(int j = 0; j < tint[i].length; ++j)
+		for(int k = 0; k < tint[i][j].length; ++k) // Iterate on Colours
 		{
-			// Subimage of Spritesheet (x,y,w,h)
-			temImg2 = temImg.getSubimage(j * Block.getLen(), 0, Block.getLen(), Block.getLen());
-			// New Blank Buffered Image
-			demo[j] = new BufferedImage(temImg2.getWidth(), temImg2.getHeight(), BufferedImage.TYPE_INT_ARGB);
-			g2D = demo[j].createGraphics();
-			g2D.drawImage(temImg2, 0, 0, null);
+			tint[i][j][k] = new BufferedImage(tiles[i][j].getWidth(), tiles[i][j].getHeight(), BufferedImage.TYPE_INT_ARGB);
+			g2D = tint[i][j][k].createGraphics();
+			g2D.drawImage(tiles[i][j], 0, 0, null);
+			g2D.setComposite(AlphaComposite.SrcAtop); // Composes (src in des) onto destination
+			g2D.setColor(tintBG[k]); // Opacity: 100 / 255
+			g2D.fillRect(0, 0, tint[i][j][k].getWidth(), tint[i][j][k].getHeight());
 			g2D.dispose();
 		}
-		// Tints an Image
-		tint = new BufferedImage[2][4];
-		for(int i = 0; i < tint.length; ++i)
-			for(int j = 0; j < tint[i].length; ++j)
-			{
-				tint[i][j] = new BufferedImage(demo[i].getWidth(), demo[i].getHeight(), BufferedImage.TYPE_INT_ARGB);
-				g2D = tint[i][j].createGraphics();
-				g2D.drawImage(demo[i], 0, 0, null);
-				g2D.setComposite(AlphaComposite.SrcAtop); // Composites (src in des) onto destination
-				g2D.setColor(tintBG[j]); // Opacity: 100 / 255 Percent
-				g2D.fillRect(0, 0, tint[i][j].getWidth(), tint[i][j].getHeight());
-				g2D.dispose();
-			}
 	}	// end method loadAll
+
+	private static void loadSubimage(BufferedImage[] bi, int idx, int x, int y, int w, int h)
+	{	// Subimage of Spritesheet (x,y,w,h)
+		temImg2 = temImg.getSubimage(x, y, w, h);
+		// New Blank Buffered Image
+		bi[idx] = new BufferedImage(temImg2.getWidth(), temImg2.getHeight(), BufferedImage.TYPE_INT_ARGB);
+		g2D = bi[idx].createGraphics();
+		g2D.drawImage(temImg2, 0, 0, null);
+		g2D.dispose();
+	}	// end method loadSubimage
 }	// end class Images
