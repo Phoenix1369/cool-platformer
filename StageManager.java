fbc8c6e608db31782b6cee0873b7839b2533a7aa
unyieldingphoenix <patrick.au@student.tdsb.on.ca>
2017-06-09 13:00:46 -0400

    Add name of previous level loaded

--- StageManager.java
+++ StageManager.java
@@ -7,60 +7,81 @@
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
+// import java.nio.file.Path;
 import java.util.StringTokenizer;
 import javax.swing.*;
 
 class StageManager 
-{	
+{
 	static BufferedReader in;
 	static BufferedWriter out;
 	static StringTokenizer st;
-	
+
+	static String lastDir  = new File("include", "levels").getPath();
+	static String lastFile = "default.txt";
+
 	public static String gets() throws IOException 
 	{
 		while(st==null || !st.hasMoreTokens())
-		{
-			String ss = in.readLine().trim();
-			st = new StringTokenizer(ss);
-		}
+			st = new StringTokenizer(in.readLine().trim());
 		return st.nextToken();
 	}
-	
-	public static void loadMap(Block[][] blockArr) throws IOException 
+
+	public static void loadMap(Block[][] blockArr)
 	{
-		// Load level
-		in = new BufferedReader(new FileReader(new File("Map.txt")));
-		for(int i = 0; i < blockArr.length; i++)
-			for(int j = 0; j < blockArr[i].length; j++)
-				blockArr[i][j].setBlock(Integer.parseInt(gets()));
-		for(int i = 0; i < blockArr.length; i++)
-			for(int j = 0; j < blockArr[i].length; j++)
-				blockArr[i][j].setField(Integer.parseInt(gets()));
-		in.close();
+		loadMap(new File(lastDir).getPath(), lastFile, blockArr);
+	}	// end method loadMap
+
+	public static void loadMap(final String dir, final String file, final Block[][] blockArr)
+	{	// Load level
+		try
+		{
+			in = new BufferedReader(new FileReader(new File(dir, file)));
+			for(int i = 0; i < blockArr.length; i++)
+				for(int j = 0; j < blockArr[i].length; j++)
+					blockArr[i][j].setBlock(Integer.parseInt(gets()));
+			for(int i = 0; i < blockArr.length; i++)
+				for(int j = 0; j < blockArr[i].length; j++)
+					blockArr[i][j].setField(Integer.parseInt(gets()));
+			in.close();
+			// Save Filenames - remembers last level
+			lastDir = dir;
+			lastFile = file;
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
 	}	// end method loadMap
 	
-	public static void saveMap(Block[][] blockArr) throws IOException 
-	{
-		// Save level
-		out = new BufferedWriter(new FileWriter(new File("Map.txt")));
-		for(int i = 0; i < blockArr.length; i++)
+	public static void saveMap(final String dir, final String file, final Block[][] blockArr)
+	{	// Save level
+		try
 		{
-			for(int j = 0; j < blockArr[i].length; j++)
+			out = new BufferedWriter(new FileWriter(new File(dir, file)));
+			for(int i = 0; i < blockArr.length; i++)
 			{
-				out.write(blockArr[i][j].getBlock() + " ");
+				for(int j = 0; j < blockArr[i].length; j++)
+				{
+					out.write(blockArr[i][j].getBlock() + " ");
+				}
+				out.newLine();
 			}
 			out.newLine();
-		}
-		out.newLine();
-		for(int i = 0; i < blockArr.length; i++)
-		{
-			for(int j = 0; j < blockArr[i].length; j++)
+			for(int i = 0; i < blockArr.length; i++)
 			{
-				out.write(blockArr[i][j].getField() + " ");
+				for(int j = 0; j < blockArr[i].length; j++)
+				{
+					out.write(blockArr[i][j].getField() + " ");
+				}
+				out.newLine();
 			}
-			out.newLine();
+			out.close();
+			System.out.println("Saved");
 		}
-		out.close();
-		System.out.println("Saved");
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
 	}	
 }	// end class StageManager
