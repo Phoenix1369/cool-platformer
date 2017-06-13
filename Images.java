ad762ed1581502049493f73aa6f6befd6435e38f
unyieldingphoenix <patrick.au@student.tdsb.on.ca>
2017-06-13 12:39:24 -0400

    Add inverted and rotated sprites

--- Images.java
+++ Images.java
@@ -12,9 +12,9 @@ import java.io.File;
 
 import javax.imageio.*;
 
-class Images 
+class Images
 {	// Loads Image Sets
-	public static BufferedImage[] sprites;
+	public static BufferedImage[][][] sprites;
 	public static BufferedImage[][] tiles;
 	public static BufferedImage[][][] tint; // tint[i][j][k]: tiles[i][j] tintBG[k]
 	public static final Color[] tintBG = {
@@ -22,16 +22,25 @@ class Images
 		new Color(255, 255, 255,   0), new Color(  0, 255, 0, 100)
 	};
 	public static final String imgDir = new File("include", "demo").getPath();
+	public static final int PIX = 4; // RGBA Channels(4)
+	// Transformation Constants
+	public static final int INV = 0, ROT = 1;
 	// Temporary Variables
 	private static BufferedImage temImg, temImg2;
 	private static Graphics2D g2D;
 
-	public static void loadAll() throws IOException 
+	public static void loadAll() throws IOException
 	{	// Loads Sprites
-		sprites = new BufferedImage[2];
+		sprites = new BufferedImage[2][4][2];
 		temImg = ImageIO.read(new File(imgDir, "sprites.png"));
 		for(int i = 0; i < sprites.length; ++i)
-			loadSubimage(sprites, i, 0, i * Block.getLen(), Block.getLen(), Block.getLen());
+		{
+			loadSubimage(sprites[i][Entity.DOWN], 1, 0, i * Block.getLen(), Block.getLen(), Block.getLen());
+			mirror(sprites[i][Entity.DOWN], 0, sprites[i][Entity.DOWN][1], Block.getLen(), Block.getLen(), INV);
+			for(int j = 1; j < 4; ++j)
+				for(int k = 0; k < 2; ++k)
+					mirror(sprites[i][(Entity.DOWN+j) % 4], k, sprites[i][(Entity.DOWN+j-1) % 4][k], Block.getLen(), Block.getLen(), ROT);
+		}
 		// Loads Tiles
 		tiles = new BufferedImage[2][2];
 		temImg = ImageIO.read(new File(imgDir, "tiles.png"));
@@ -41,26 +50,47 @@ class Images
 		// Tints the Tiles
 		tint = new BufferedImage[2][2][4];
 		for(int i = 0; i < tint.length; ++i)
-		for(int j = 0; j < tint[i].length; ++j)
-		for(int k = 0; k < tint[i][j].length; ++k) // Iterate on Colours
-		{
-			tint[i][j][k] = new BufferedImage(tiles[i][j].getWidth(), tiles[i][j].getHeight(), BufferedImage.TYPE_INT_ARGB);
-			g2D = tint[i][j][k].createGraphics();
-			g2D.drawImage(tiles[i][j], 0, 0, null);
-			g2D.setComposite(AlphaComposite.SrcAtop); // Composes (src in des) onto destination
-			g2D.setColor(tintBG[k]); // Opacity: 100 / 255
-			g2D.fillRect(0, 0, tint[i][j][k].getWidth(), tint[i][j][k].getHeight());
-			g2D.dispose();
-		}
+			for(int j = 0; j < tint[i].length; ++j)
+				for(int k = 0; k < tint[i][j].length; ++k) // Iterate on Colours
+				{
+					tint[i][j][k] = new BufferedImage(tiles[i][j].getWidth(), tiles[i][j].getHeight(), BufferedImage.TYPE_INT_ARGB);
+					g2D = tint[i][j][k].createGraphics();
+					g2D.drawImage(tiles[i][j], 0, 0, null);
+					g2D.setComposite(AlphaComposite.SrcAtop); // Composes (src in des) onto destination
+					g2D.setColor(tintBG[k]); // Opacity: 100 / 255
+					g2D.fillRect(0, 0, tint[i][j][k].getWidth(), tint[i][j][k].getHeight());
+					g2D.dispose();
+				}
 	}	// end method loadAll
 
 	private static void loadSubimage(BufferedImage[] bi, int idx, int x, int y, int w, int h)
 	{	// Subimage of Spritesheet (x,y,w,h)
 		temImg2 = temImg.getSubimage(x, y, w, h);
 		// New Blank Buffered Image
-		bi[idx] = new BufferedImage(temImg2.getWidth(), temImg2.getHeight(), BufferedImage.TYPE_INT_ARGB);
+		bi[idx] = new BufferedImage(temImg2.getWidth(), temImg2.getHeight(), BufferedImage.TYPE_4BYTE_ABGR);
 		g2D = bi[idx].createGraphics();
 		g2D.drawImage(temImg2, 0, 0, null);
 		g2D.dispose();
 	}	// end method loadSubimage
+
+	public static void mirror(BufferedImage[] des, int dx, final BufferedImage src, int w, int h, int flag)
+	{	// Clones a transformed 'src' into 'des[dx]'
+		// Java's DataBufferByte is faster than AffineTransform and img.getRGB(row,col)
+		des[dx] = new BufferedImage(w, h, src.getType());
+		byte[] dpx = ((DataBufferByte)des[dx].getRaster().getDataBuffer()).getData();
+		byte[] spx = ((DataBufferByte)src.getRaster().getDataBuffer()).getData();
+		int cur, ptr=0;
+
+		for(int i = 0; i < h; ++i)
+			for(int j = 0; j < w; ++j)
+			{
+				cur = (i * w + j) * PIX;
+				if(flag==INV)
+					ptr = (i * w + (w-j-1)) * PIX; // Horizontally inverts 'src'
+				else if(flag==ROT)
+					ptr = ((w-j-1) * w + i) * PIX;
+				for(int k = 0; k < 4; ++k)
+					dpx[cur+k] = spx[ptr+k]; // Channels: ABGR
+			}
+	}	// end method mirror
 }	// end class Images
