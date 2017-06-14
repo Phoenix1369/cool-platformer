faec69db9d9c59f173b5a86775008a7f752d345a
James L <jameslong0410@gmail.com>
2017-06-14 19:41:50 -0400

    Add win and lose screens

---
+++ WinScreen.java
@@ -0,0 +1,45 @@
+/******
+* name: Patrick Au, James Long
+* date: March 2017
+* code: ICS4U1
+* note: Win Screen
+*******/
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.*;
+import java.io.*;
+import java.util.StringTokenizer;
+import javax.swing.*;
+import javax.imageio.ImageIO;
+
+class WinScreen extends JPanel implements ActionListener
+{
+	JButton title;
+	WinScreen(Dimension dim)
+	{
+		setLayout(new BorderLayout());
+		
+		try
+		{
+			BufferedImage insImage = ImageIO.read(new File(System.getProperty("user.dir") + "/include/demo/winImage.png"));
+			JLabel insGraphic = new JLabel(new ImageIcon(insImage));
+			add(insGraphic, BorderLayout.CENTER);
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+		
+		title = new JButton("Back to Title");
+		title.addActionListener(this);
+		add(title, BorderLayout.SOUTH);
+	}	// end constructor(Dimension)
+	
+	public void actionPerformed(ActionEvent ae)
+	{
+		if(ae.getSource() == title)
+		{
+			CoolPlatformer.changeScreen("TitleScreen");
+		}
+	}	// end method actionPerformed
+}	// end class WinScreen
