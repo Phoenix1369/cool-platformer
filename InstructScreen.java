1b02960245a8520a7b3795e0795e517e6e3c8cb2
James L <jameslong0410@gmail.com>
2017-06-13 22:16:45 -0400

    Change visuals

--- InstructScreen.java
+++ InstructScreen.java
@@ -6,18 +6,33 @@
 *******/
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.*;
 import java.io.*;
 import java.util.StringTokenizer;
 import javax.swing.*;
+import javax.imageio.ImageIO;
 
 class InstructScreen extends JPanel implements ActionListener
 {
 	JButton resume;
 	InstructScreen(Dimension dim)
 	{
+		setLayout(new BorderLayout());
+		
+		try
+		{
+			BufferedImage insImage = ImageIO.read(new File(System.getProperty("user.dir") + "/include/demo/instructImage.png"));
+			JLabel insGraphic = new JLabel(new ImageIcon(insImage));
+			add(insGraphic, BorderLayout.CENTER);
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+		
 		resume = new JButton("Return");
 		resume.addActionListener(this);
-		add(resume);
+		add(resume, BorderLayout.SOUTH);
 	}	// end constructor(Dimension)
 	
 	public void actionPerformed(ActionEvent ae)
