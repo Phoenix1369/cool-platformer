743c42fc5f5654dc9c5cbcd2e99bf037a5d34fa7
James L <jameslong0410@gmail.com>
2017-06-13 22:22:05 -0400

    Change visuals

--- PauseScreen.java
+++ PauseScreen.java
@@ -6,21 +6,39 @@
 *******/
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.*;
 import java.io.*;
 import java.util.StringTokenizer;
 import javax.swing.*;
+import javax.imageio.ImageIO;
 
 class PauseScreen extends JPanel implements ActionListener
 {
 	JButton resume, title;
 	PauseScreen(Dimension dim)
 	{
+		setLayout(new BorderLayout());
+		
+		try
+		{
+			BufferedImage pauseImage = ImageIO.read(new File(System.getProperty("user.dir") + "/include/demo/pauseImage.png"));
+			JLabel pauseGraphic = new JLabel(new ImageIcon(pauseImage));
+			add(pauseGraphic, BorderLayout.CENTER);
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+		
+		JPanel btnPanel = new JPanel(new GridLayout(1, 2));
+		
 		resume = new JButton("Resume");
 		title = new JButton("Back to Title");
 		resume.addActionListener(this);
 		title.addActionListener(this);
-		add(resume);
-		add(title);
+		btnPanel.add(resume);
+		btnPanel.add(title);
+		add(btnPanel, BorderLayout.SOUTH);
 	}	// end constructor(Dimension)
 	
 	public void actionPerformed(ActionEvent ae)
