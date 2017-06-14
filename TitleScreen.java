9efc8856ff04d290a698fe83e1ebfbbafd7f9d60
James L <jameslong0410@gmail.com>
2017-06-13 21:48:09 -0400

    Change Title Screen to use an image

--- TitleScreen.java
+++ TitleScreen.java
@@ -6,16 +6,31 @@
 *******/
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.*;
 import java.io.*;
 import java.util.StringTokenizer;
 import javax.swing.*;
+import javax.imageio.ImageIO;
 
 class TitleScreen extends JPanel implements ActionListener
 {
 	JButton start, editor, instruct;
+	Dimension RA = new Dimension(0, 10);
 	TitleScreen(Dimension dim)
 	{
-		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		setLayout(new BorderLayout());
+		
+		try
+		{
+			BufferedImage titleImage = ImageIO.read(new File(System.getProperty("user.dir") + "/include/demo/titleImage.png"));
+			JLabel titleGraphic = new JLabel(new ImageIcon(titleImage));
+			add(titleGraphic, BorderLayout.CENTER);
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+		
 		start = new JButton("Start Game");
 		instruct = new JButton("Instructions");
 		editor = new JButton("Level Editor");
@@ -25,9 +40,11 @@ class TitleScreen extends JPanel implements ActionListener
 		start.addActionListener(this);
 		instruct.addActionListener(this);
 		editor.addActionListener(this);
-		add(start);
-		add(instruct);
-		add(editor);		
+		JPanel btnPanel = new JPanel(new GridLayout(1,3));
+		btnPanel.add(start);
+		btnPanel.add(instruct);
+		btnPanel.add(editor);
+		add(btnPanel, BorderLayout.SOUTH);
 	}	// end constructor(Dimension)
 	
 	public void actionPerformed(ActionEvent ae)
