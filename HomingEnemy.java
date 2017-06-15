131fb6003445335964de06cae88e17e331009959
unyieldingphoenix <patrick.au@student.tdsb.on.ca>
2017-06-14 23:46:27 -0400

    Remove bfs method

--- HomingEnemy.java
+++ HomingEnemy.java
@@ -5,28 +5,15 @@
 * note: HomingEnemy character - floats around obstacles targeting Player
 *******/
 import java.awt.*;
-import java.util.ArrayDeque;
 
 class HomingEnemy extends Enemy
 {
-	private static final int INFN = 0x3F3F3F3F;
-	private static final int delayLim = GameScreen.FPS * 3 / 4;
-	private static int[][] moves = { { 0, -1 }, { +1, 0 }, { 0, +1 }, { -1, 0 } };
-
-	private Dimension cur, idx, nxt;
-	private ArrayDeque<Dimension> Q;
-
-	private int[][] grid;
-	private int bfsDelay;
+	private Dimension idx, nxt;
 
 	HomingEnemy(double x, double y)
 	{
 		super(x, y);
 		this.M_SPD = 160.0 / GameScreen.FPS;
-		this.bfsDelay = delayLim;
-		cur = GameScreen.getDlen();
-		grid = new int[cur.height][cur.width];
-		Q = new ArrayDeque<Dimension>();
 	}	// end constructor(double,double)
 
 	public void advance()
@@ -35,11 +22,6 @@ class HomingEnemy extends Enemy
 		// Directions are RELATIVE here ie. "R" in "Up" Field <--> "L" in "Down" (Normal) Field
 		updateField();
 		idx = getIdx(); // Current Array Index Position
-		if(++bfsDelay >= delayLim)
-		{	// Don't BFS every single tick
-			bfs();
-			bfsDelay = 0;
-		}
 		if(!vx)
 		{	// Reset X
 			setKey(LEFT, false);
@@ -52,44 +34,19 @@ class HomingEnemy extends Enemy
 		}
 		for(int k = 0; k < 4; ++k)
 		{
-			nxt = new Dimension(idx.width+moves[k][0], idx.height+moves[k][1]);
-			if(invalid(nxt)) continue;
-			if(grid[nxt.height][nxt.width] <= grid[idx.height][idx.width])
+			nxt = new Dimension(idx.width+Player.moves[k][0], idx.height+Player.moves[k][1]);
+			if(Player.invalid(nxt)) continue;
+			if(Player.getG()[nxt.height][nxt.width] <= Player.getG()[idx.height][idx.width])
 				setKey(k, true); // Picks tile closer to Player
 		}
 		if(keysPressed[UP] && keysPressed[DOWN])
-			setKey((grid[idx.height-1][idx.width] < grid[idx.height+1][idx.width]) ? DOWN  :   UP, false);
+			setKey((Player.getG()[idx.height-1][idx.width] < Player.getG()[idx.height+1][idx.width]) ? DOWN  :   UP, false);
 		if(keysPressed[RIGHT] && keysPressed[LEFT])
-			setKey((grid[idx.height][idx.width-1] < grid[idx.height][idx.width+1]) ? RIGHT : LEFT, false);
+			setKey((Player.getG()[idx.height][idx.width-1] < Player.getG()[idx.height][idx.width+1]) ? RIGHT : LEFT, false);
 		vx = vy = false;
 		updateVectors();
 		move(this.vel);
 	}	// end method advance
-
-	public void bfs()
-	{	// Performs a breadth-first search
-		Q.clear();
-		for(int i = 0; i < GameScreen.getDlen().height; ++i)
-			for(int j = 0; j < GameScreen.getDlen().width; ++j)
-				grid[i][j] = INFN; // Initializes the grid
-		Dimension src = GameScreen.getP().getIdx();
-		grid[src.height][src.width] = 0;
-		Q.add(src);
-		while(!Q.isEmpty())
-		{
-			cur = Q.poll();
-			for(int k = 0; k < 4; ++k)
-			{
-				nxt = new Dimension(cur.width+moves[k][0], cur.height+moves[k][1]);
-				if(invalid(nxt)) continue;
-				if(grid[nxt.height][nxt.width] > grid[cur.height][cur.width]+1)
-				{	// Compute distance to new tile
-					grid[nxt.height][nxt.width] = grid[cur.height][cur.width] + 1;
-					Q.offer(nxt);
-				}
-			}
-		}
-	}	// end method bfs
 
 	@Override // Superclass: Entity
 	public void draw(Graphics g)
@@ -97,13 +54,6 @@ class HomingEnemy extends Enemy
 		g2D = (Graphics2D)g;
 		g2D.drawImage(Images.sprites[2][ DOWN ][ movingRel(LEFT)?0:1 ], this.x, this.y, lenB, lenB, null);
 	}	// end method draw
-
-	public boolean invalid(final Dimension d)
-	{	// Dimension goes out of bounds, or wall block
-		return	(d.width < 0) || (d.width >= GameScreen.getDlen().width-GameScreen.edW) ||
-			(d.height < 0) || (d.height >= GameScreen.getDlen().height-GameScreen.edW) ||
-			(GameScreen.getBlocks(d.height, d.width).getBlock() == Block.EARTH);
-	}	// end method invalid
 
 	@Override // Superclass: Entity
 	public void setKey(int indexToSet, boolean pressedDown)
